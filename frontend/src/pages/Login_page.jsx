3e02fd25b47cd48d8b0ce1337599f687596fdd25
Ayush <ayushkumar44344@gmail.com>
2026-03-06 14:20:32 +0530

    Update Login_page.jsx

--- frontend/src/pages/Login_page.jsx
+++ frontend/src/pages/Login_page.jsx
@@ -1,7 +1,6 @@
 
 import Sidebar_login from '../components/sidebar_login'
 import Login_left from '../components/Login_left'
-import Login_box from '../components/Login_box'
 const Login_page = () => {
     return (
         <div >
