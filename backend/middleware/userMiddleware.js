fd2c904f36818d63ee5c3d29c7c85090bd208341
Ayush <ayushkumar44344@gmail.com>
2026-03-06 13:59:50 +0530

    Update userMiddleware.js

--- backend/middleware/userMiddleware.js
+++ backend/middleware/userMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const redisclient = require("../config/redis");
 
 const userMiddleware = async (req, res,next) => {
