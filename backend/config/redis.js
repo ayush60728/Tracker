10f3bfee9474b59c1e5b797136796bd79fc8bb0e
Ayush <ayushkumar44344@gmail.com>
2026-05-06 22:02:48 +0530

    Update redis.js

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,15 +1,12 @@
-const { createClient } = require('redis');
+const { createClient } = require("redis");
 require("dotenv").config();
 
 const redisclient = createClient({
-    username: 'default',
-    password: process.env.REDIS_PASS,
-    socket: {
-        host: 'redis-16587.crce217.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 16587
-        tls: true
-    }
+    url: process.env.REDIS_URL
 });
 
+redisclient.on("error", (err) => {
+    console.log("Redis Error:", err.message);
+});
 
 module.exports = redisclient;
